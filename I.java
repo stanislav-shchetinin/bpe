abd9f3b60f1c20e95f59fa4aca7308c0864c2db8
Stas Shchetinin <stas.shc@gmail.com>
2024-03-09 16:52:36 +0300

    r13

--- I.java
+++ I.java
@@ -31,4 +31,8 @@ public class I implements K {
     public int[] ii() {
         return new int[]{0, 1, 2, 3, 4};
     }
+
+    public java.util.List<String> jj() {
+        return new java.util.LinkedList<String>();
+    }
 }
